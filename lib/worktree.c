dd572ba45549991e172e2f8f7b2381ed7b7a0f42
Stefan Sperling <stsp@stsp.name>
2026-09-05 12:31:16 +0000

    fix missing status code of staged files in log message templates during commit

--- lib/worktree.c
+++ lib/worktree.c
@@ -6933,6 +6933,9 @@ got_commitable_get_path(struct got_commitable *ct)
 unsigned int
 got_commitable_get_status(struct got_commitable *ct)
 {
+	if (ct->staged_status != GOT_STATUS_NO_CHANGE)
+		return ct->staged_status;
+
 	return ct->status;
 }
 
